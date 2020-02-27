fb7f3dc95aa80936e2741851dc3dff4305907879
Gustavo <gustavo.visentini@gmail.com>
2020-02-26 23:05:33 -0300

    Version 0.0.1

---
+++ data/data.js
@@ -0,0 +1,102 @@
+/*!
+ * Guim González - Portfolio v1.0.0 (https://guimgonzalez.me)
+ * Copyright 2019 Guim González
+ * Licensed under MIT (https://github.com/GuimG/dev-portfolio/master/LICENSE)
+ */
+
+// Change your profile data here
+let profile = {
+  picture: "",
+  name: "Gustavo Visentini",
+  description: "iOS Developer and Computer Vision aplications for agriculture",
+  bio:
+    " PhD Student in Agronomy, iOS Developer and computer vision applications with extensive experience a development softwares for drone and automation using technologies such as raspberry and arduino. Master of Applied Computing and graduated in Computer Science from the University of Passo Fundo. Professor at the Santa Catarina Federal Institute IFC during 2018.",
+  email: "gustavo.visentini@gmail.com",
+  location: "Passo Fundo, Brazil",
+  birth: "2020-02-26", // YYYY-MM-DD format
+
+
+  skills: [
+    "iOS",
+    "Swift",
+    "C/C++",
+    "Java",
+    "Objective-C",
+    "HTML",
+    "Javascript",
+    "MongoDB",
+    "NodeJS",
+    "Android",
+    "Firebase",
+    "SQL",
+    "OpenCV"
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/",
+    twitter: "https://twitter.com/",
+    github: "https://github.com/gustavovisentini",
+    stackoverflow: "https://stackoverflow.com/users/2731180/zedaoreia",
+    dev: "https://dev.to/"
+  },
+  experience: [
+    {
+      title: "Professor",
+      company: "Federal Institute Catarinense",
+      url: "https://concordia.ifc.edu.br/",
+      location: "Concórdia, Brazil",
+      description:
+        "Professor of computer courses and related areas.",
+      from: "Jan 2018",
+      to: "Jan 2019"
+    },
+    {
+      title: "Developer",
+      company: "Freelancer",
+      url: "https://gustavovisentini.github.io",
+      location: "Passo Fundo, Brazil",
+      description:
+        "iOS, Computer Vision and Drones applications developer ",
+      from: "Jan 2011",
+      to: "Now"
+    }
+  ],
+  education: [
+    {
+      school: "University of Passo Fundo",
+      degree: "Computer Science",
+      fieldOfStudy: "Computer Science",
+      location: "Passo Fundo, Brazil",
+      from: "Mar. 2011",
+      to: "Jul. 2015"
+    },
+    {
+      school: "University of Passo Fundo",
+      degree: "Master in Aplied Computing",
+      fieldOfStudy: "Computer Science",
+      location: "Passo Fundo, Brazil",
+      from: "Mar. 2016",
+      to: "Dez. 2018"
+    },
+    {
+      school: "University of Passo Fundo",
+      degree: "PhD in Agronomy",
+      fieldOfStudy: "Agronomy",
+      location: "Passo Fundo, Brazil",
+      from: "Mar. 2020",
+      to: "Now"
+    }
+  ],
+  quote: "It's just a code there"
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
